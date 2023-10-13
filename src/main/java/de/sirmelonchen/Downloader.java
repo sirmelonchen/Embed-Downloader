6942e01297d0a837ef27ef3841c9ff31c13531c4
sirmelonchen <68815284+sirmelonchen@users.noreply.github.com>
2023-10-13 09:20:53 +0200

    update

--- src/main/java/de/sirmelonchen/Downloader.java
+++ src/main/java/de/sirmelonchen/Downloader.java
@@ -7,41 +7,8 @@ import java.nio.channels.Channels;
 import java.nio.channels.ReadableByteChannel;
 public class Downloader {
     public static void main() throws IOException {
-        String url = Main.getConfig("URL");
 
-        try {
-            downloadUsingNIO(url, Main.getConfig("Path"));
-
-            downloadUsingStream(url, Main.getConfig("Path"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        delurl();
     }
-
-    private static void downloadUsingStream(String urlStr, String file) throws IOException{
-        URL url = new URL(urlStr);
-        BufferedInputStream bis = new BufferedInputStream(url.openStream());
-        FileOutputStream fis = new FileOutputStream(file);
-        byte[] buffer = new byte[1024];
-        int count=0;
-        while((count = bis.read(buffer,0,1024)) != -1)
-        {
-            fis.write(buffer, 0, count);
-        }
-        fis.close();
-        bis.close();
-    }
-
-    private static void downloadUsingNIO(String urlStr, String file) throws IOException {
-        URL url = new URL(urlStr);
-        ReadableByteChannel rbc = Channels.newChannel(url.openStream());
-        FileOutputStream fos = new FileOutputStream(file);
-        fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
-        fos.close();
-        rbc.close();
-    }
-
     private static void delurl() throws IOException {
         Main.delConfig("URL");
     }
